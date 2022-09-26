2f1f26f0ae3fc7a673762e22f466e6134f445cc3
Ronald Dogomeo <ronald.dogomeo@mobilemoney.ph>
2022-09-27 01:41:43 +0800

    init commit

--- src/components/LayoutComponents/Applicant/DrawerItems.vue
+++ src/components/LayoutComponents/Applicant/DrawerItems.vue
@@ -2,6 +2,7 @@
   <div>
     <div v-for="(item,key) in drawerItems" :key="key">
       <q-expansion-item
+        class="nav-item"
         dense-toggle
         expand-separator
         :icon="item.icon"
@@ -13,13 +14,17 @@
           :to="i.path"
           exact
         >
-          <q-item-section avatar>
+          <q-item-section avatar class="drawer-item-icon">
             <i :class="i.icon"></i>
           </q-item-section>
 
           <q-item-section class="text-red-15">
             {{ i.label || "N/A" }}
           </q-item-section>
+          <q-item-section avatar>
+            <q-badge class="drawer-badge" rounded  color="green" text-color="black" :label="i.count" />
+          </q-item-section>
+
         </q-item>
       </q-expansion-item>
 
@@ -52,19 +57,19 @@ export default {
           label: "S1 Applications",
           children: [
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "Application List",
               count: 0,
               path: "/asc/page/application/s1/lists",
             },
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "Multiple Applications",
               count: 0,
               path: "/asc/page/application/s1/multiple",
             },
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "Special Applications",
               count: 0,
               path: "/asc/page/application/s1/special",
@@ -76,13 +81,13 @@ export default {
           label: "S2 Applications",
           children: [
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "Application List",
               count: 0,
               path: "/asc/page/application/s2/lists",
             },
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "Special Clearing Request",
               count: 0,
               path: "/asc/page/application/s2/special",
@@ -94,25 +99,25 @@ export default {
           label: "For Payments",
           children: [
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "Individual",
               count: 0,
               path: "/asc/page/application/single-application",
             },
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "Multiple",
               count: 0,
               path: "/asc/page/application/multiple-applications",
             },
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "S2 Special Clearing",
               count: 0,
               path: "/asc/page/application/special-applications",
             },
             {
-              icon: "fa-solid fa-chevron-right",
+              icon: "fa-solid fa-caret-right",
               label: "Accounts Payable",
               count: 0,
               path: "/asc/page/application/accounts-payable",
